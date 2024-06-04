63bd5eb988aebaa23c7b71736ee5d9c93339ee7f
Daniel Song <daniel.song1909@gmail.com>
2024-06-03 21:47:21 -0400

    Camera Movement!!!
    Smoothened out Camera Movement by scripting it, basically just made it so that camera follows head position but a little bit behind and above (a set customizable value).

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float distanceX = 0.0f; 
+    public float distanceZ = 0.0f; 
+    public float distanceY = 0.0f; 
+    public GameObject reference; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(reference.transform.position.x + distanceX, reference.transform.position.y + distanceY, reference.transform.position.z + distanceZ); 
+    }
+}
