a01eac137234032555eacff6e760b7bfb0eeaf3a
Daniel Song <daniel.song1909@gmail.com>
2024-06-05 12:01:21 -0400

    did some stuff

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController 
+public class EnemyController : MonoBehaviour
 {
     //TYPE OF ENEMY 
     public bool isRanged = false; 
@@ -13,6 +13,12 @@ public class EnemyController
     protected bool movingRight = false; 
     protected bool movingLeft = false; 
 
+    //ENEMY ROTATION 
+    public Transform target; 
+    private Quaternion targetRotation; 
+    //protected Quaternion gradualRotation; WORK FROM HERE" PROBLEM IS THAT I T AUTOMATICALLT SANAPAPS TO ROTATIATION
+    public float rotationSpeed = 5f; 
+
     //ATTACK & PARRY DECISIONS 
 
     // Update is called once per frame
@@ -21,7 +27,8 @@ public class EnemyController
         if (isRanged) {
             //isRanged ai is here, controls everything 
         } else {
-            //if is melee ai is here, controls everything 
+            targetRotation = Quaternion.Euler(transform.rotation.x, target.rotation.y-transform.rotation.y, transform.rotation.z); 
+
         }
     }
 }
