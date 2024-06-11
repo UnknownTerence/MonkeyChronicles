1fb4558d86c20edb48d88e7f5ac28bc5bb7b941e
UnknownTerence <69740024+UnknownTerence@users.noreply.github.com>
2024-06-10 22:25:13 -0400

    sword stuff fixed a bit

--- Assets/Scripts/SwordArmMovement.cs
+++ Assets/Scripts/SwordArmMovement.cs
@@ -21,41 +21,41 @@ public class SwordArmMovement : MonoBehaviour
     {
         
         //charging up attack
-        if (Input.GetKey(KeyCode.K)) { //charging up right attack 
+        if (Input.GetMouseButton(0)) { //charging up right attack 
             charge+=Time.deltaTime*2; 
         } 
 
         //determining of stage 
         if (charge==0) armAngle.targetRotation = Quaternion.Euler(new Vector3(0, 0, 0)); 
         else if (charge<2) {
-            armAngle.targetRotation = Quaternion.Euler(new Vector3(300, 0, 0)); 
+            armAngle.targetRotation = Quaternion.Euler(new Vector3(300, 0, 15)); 
         } else if (charge<4) { 
-            armAngle.targetRotation = Quaternion.Euler(new Vector3(25, 0, 0)); 
+            armAngle.targetRotation = Quaternion.Euler(new Vector3(25, 0, 15)); 
         } else if (charge<6) { 
-            armAngle.targetRotation = Quaternion.Euler(new Vector3(50, 0, 0)); 
+            armAngle.targetRotation = Quaternion.Euler(new Vector3(50, 0, 15)); 
         } else {
-            armAngle.targetRotation = Quaternion.Euler(new Vector3(100, 0, 0)); 
+            armAngle.targetRotation = Quaternion.Euler(new Vector3(75, 0, 15)); 
         } 
 
         //sword movement
-        if ((!Input.GetKey(KeyCode.K)) && charge>0) {
+        if ((!Input.GetMouseButton(0)) && charge>0) {
             if (charge>6) finalCharge = 6.0f;
             else finalCharge = charge; 
             if (charge<2) {
                 armAngle.targetRotation = Quaternion.Euler(new Vector3(0, 330, 0)); 
-                thrust.AddForce(-thrust.transform.right * 10 * configurationFactor); 
+                thrust.AddForce(-thrust.transform.forward * 10 * configurationFactor); 
                 animationDuration = 0.5f; 
             } else if (charge<4) {
                 armAngle.targetRotation = Quaternion.Euler(new Vector3(0, 350, 0)); 
-                thrust.AddForce(-thrust.transform.right * 20 * configurationFactor);
+                thrust.AddForce(-thrust.transform.forward * 20 * configurationFactor);
                 animationDuration = 1.0f; 
             } else if (charge<6) {
                 armAngle.targetRotation = Quaternion.Euler(new Vector3(0, 350, 0)); 
-                thrust.AddForce(-thrust.transform.right * 40 * configurationFactor);
+                thrust.AddForce(-thrust.transform.forward * 40 * configurationFactor);
                 animationDuration = 1.5f;
             } else if (charge>6) {
                 armAngle.targetRotation = Quaternion.Euler(new Vector3(0, 350, 0)); 
-                thrust.AddForce(-thrust.transform.right * 80 * configurationFactor);
+                thrust.AddForce(-thrust.transform.forward * 80 * configurationFactor);
                 thrust.AddForce(thrust.transform.up * configurationFactor/4);
                 animationDuration = 2.0f; 
             }
